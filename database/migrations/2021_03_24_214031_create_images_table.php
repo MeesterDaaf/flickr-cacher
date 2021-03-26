a6ccbd918b2c509b79deb26213683202347508a4
meesterdaaf <d.vanoudheusden@rocva.nl>
2021-03-26 23:35:01 +0100

    added migrations

---
+++ database/migrations/2021_03_24_214031_create_images_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->string('url');
+            $table->integer('views')->nullable();
+            $table->string('owner')->nullable();
+            $table->string('location')->nullable();
+            $table->string('date_taken')->nullable();
+            $table->string('image');
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
