9064e90546f0906e34cc1f5d267d1f38870f6643
meesterdaaf <d.vanoudheusden@rocva.nl>
2021-03-26 23:34:09 +0100

    model created

---
+++ app/Models/CsvData.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CsvData extends Model
+{
+    use HasFactory;
+
+
+    protected $table = 'images';
+    public $timestamps = false;
+}
